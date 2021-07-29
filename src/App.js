a4bda8fea9565af1a4733d3cf5d9bd7ce6963c66
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-07-29 17:12:33 +0500

    seting basic themes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import WebFont from "webfontloader";
+import { GlobalStyles } from "./theme/GlobalStyles";
+import { useTheme } from "./theme/useTheme";
+
+// 2: Create a cotainer
+const Container = styled.div`
+  margin: 15px 15px 15px 15px;
+`;
 
 function App() {
+  // 3: Get the selected theme, font list, etc.
+  const { theme, themeLoaded, getFonts } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [themeLoaded]);
+
+  // 4: Load all the fonts
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts(),
+      },
+    });
+  });
+
+  // 5: Render if the theme is loaded.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {themeLoaded && (
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles />
+          <Container style={{ fontFamily: selectedTheme.font }}>
+            <h1>Hello World</h1>
+            <p>For Exciting Projects</p>
+            <a href="http://github.com/Coder-Aht007/" target="_blank">
+              Click here.
+            </a>
+          </Container>
+        </ThemeProvider>
+      )}
+    </>
   );
 }
 
---
+++ src/theme/useTheme.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { setToLS, getFromLS } from '../utils/storage';
+import _ from 'lodash';
+
+export const useTheme = () => {
+  const themes = getFromLS('my-themes');
+  const [theme, setTheme] = useState(themes.data.light);
+  const [themeLoaded, setThemeLoaded] = useState(false);
+
+  const setMode = mode => {
+    setToLS('theme', mode)
+    setTheme(mode);
+  };
+
+  const getFonts = () => {
+    const allFonts = _.values(_.mapValues(themes.data, 'font'));
+    return allFonts;
+  }
+
+  useEffect(() =>{
+    const localTheme = getFromLS('theme');
+    localTheme ? setTheme(localTheme) : setTheme(themes.data.dark);
+    setThemeLoaded(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  }, []);
+
+  return { theme, themeLoaded, setMode, getFonts };
+};
