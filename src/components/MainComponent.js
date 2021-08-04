1b072e6801a7f824b610fc95da52c69795c3156e
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-08-04 12:25:00 +0500

    routes transferred to separate file

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,35 +1,19 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import WebFont from "webfontloader";
-import { Route, Switch, Redirect } from "react-router-dom";
+import _ from "lodash";
+
 import { GlobalStyles } from "../theme/GlobalStyles";
 import Header from "./shared/header";
-import Login from "./auth/login";
-import Signup from "./auth/signup";
-import _ from "lodash";
-import * as DATA from '../theme/schema'
-import UserUtils from "./shared/user";
-import Index from './index'
-
-const isLogin = UserUtils.isLogin;
-
-const PrivateRoute = ({component: Component, ...rest}) => {
-  return (
-    
-      <Route {...rest} render={props => (
-          isLogin() ?
-              <Component {...props} />
-          : <Redirect to="/login" />
-      )} />
-  );
-};
+import * as DATA from "../theme/schema";
+import Routes from "./shared/routes";
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      theme: DATA.default.data.light
-    }
+    this.state = {
+      theme: DATA.default.data.light,
+    };
   }
 
   componentDidMount() {
@@ -41,23 +25,14 @@ export default class Main extends React.Component {
     });
   }
 
-  
   render() {
     return (
       <>
-          <ThemeProvider theme={this.state.theme}>
-            <GlobalStyles />
-            <Header />
-            <div>
-              <Switch>
-              <PrivateRoute path="/index" component={()=><Index />}  />
-              <Route exact path="/login" component={()=><Login />} />
-              <Route exact path="/signup" component={()=><Signup />} />
-              <Redirect to="/index" />
-              </Switch>
-            </div>
-          </ThemeProvider>
-
+        <ThemeProvider theme={this.state.theme}>
+          <GlobalStyles />
+          <Header />
+          <Routes />
+        </ThemeProvider>
       </>
     );
   }
